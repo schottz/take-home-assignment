73da1cf7e950e167ba35df2eb973b1bb3c550629
Felipe Schottz <felipe.schottz@memora.com.br>
2023-09-30 19:03:11 -0300

    Forgot about the build command

--- linux/automation.sh
+++ linux/automation.sh
@@ -3,6 +3,7 @@
 IMAGE_NAME="schottz/dockerize"
 TAG=$(date +"%s")
 
+docker build -t $IMAGE_NAME ../dockerize/
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$TAG
 
 sed -e "s#MY_NEW_IMAGE#$IMAGE_NAME:$TAG#g" script.yaml > new-app.yaml
