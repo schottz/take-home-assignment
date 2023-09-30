4b2e4346860aa315ce5593d518d580689514c428
Felipe Schottz <felipe.schottz@memora.com.br>
2023-09-29 22:58:56 -0300

    Solve linux

---
+++ linux/automation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE_NAME="schottz/dockerize"
+TAG=$(date +"%s")
+
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$TAG
+
+sed -e "s#MY_NEW_IMAGE#$IMAGE_NAME:$TAG#g" script.yaml > new-app.yaml
+
+kubectl diff -f ./new-app.yaml
